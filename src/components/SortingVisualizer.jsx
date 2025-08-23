687e7c94604a2f7d0760510f4a0b2c35d5461572
Bimbok <bimbokmkj@gmail.com>
2025-08-23 11:23:29 +0530

    highly-fixe-counting-sort-sobeta--algo-v3.6.0

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -673,8 +673,8 @@ export default function SortingVisualizer() {
                 <option value='quick'>Quick Sort</option>
                 <option value='merge'>Merge Sort</option>
                 <option value='heap'>Heap Sort</option>
-                <option value='counting'>Counting Sort</option>
-                <option value='radix'>Radix Sort</option>
+                <option value='counting'>Counting Sort (beta)</option>
+                <option value='radix'>Radix Sort (beta)</option>
               </select>
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.2' stroke='currentColor' className='h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700'>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9' />
