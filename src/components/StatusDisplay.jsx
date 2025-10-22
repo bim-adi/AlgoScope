813345f3489271e9a246e5c8c6cd9c0a46bc1ed9
Bimbok <bimbokmkj@gmail.com>
2025-10-22 20:00:05 +0530

    very-small-ui-fix

--- src/components/StatusDisplay.jsx
+++ src/components/StatusDisplay.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StatusDisplay = ({ message }) => {
   return (
     <>
-      <div className="bg-amber-200 border-2 rounded w-full m-auto p-3 mt-8 mb-8">
+      <div className="bg-amber-200 border-2 rounded w-full m-auto p-3 mt-8">
         <div>{message}</div>
       </div>
     </>
